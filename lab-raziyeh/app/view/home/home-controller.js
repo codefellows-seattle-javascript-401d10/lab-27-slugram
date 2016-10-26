8c3ab98fcc06823f4dff14040a4d4ace960febba
raziyeh bazargan <r.bazargan@gmail.com>
2016-10-26 14:17:03 -0700

    Delete gallery: done!

--- lab-raziyeh/app/view/home/home-controller.js
+++ lab-raziyeh/app/view/home/home-controller.js
@@ -2,9 +2,33 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', HomeController ];
+module.exports = ['$log', '$rootScope', 'galleryService', HomeController ];
 
-function HomeController($log){
+function HomeController($log, $rootScope, galleryService){
   $log.debug('init homeCtrl');
-  this.oneAtATime = true;
+  this.galleries = [];
+
+  this.fetchGalleries = function(){
+    galleryService.fetchGalleries()
+    .then( galleries => {
+      this.galleries = galleries;
+    });
+  };
+
+  this.deleteGallery = function(galleyId){
+    galleryService.deleteGallery(galleyId)
+    .then( () => {
+      this.fetchGalleries();
+      $log.debug('delete gallery by Id');
+    });
+  };
+
+  // when the controller gets created fetchGalleries
+  this.fetchGalleries();
+
+  // when the locationChangeSuccess fetchGalleries
+  $rootScope.$on('$locationChangeSuccess', () => {
+    this.fetchGalleries();
+  });
+
 }
