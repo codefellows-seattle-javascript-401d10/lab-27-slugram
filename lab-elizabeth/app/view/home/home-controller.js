b66942f362760bbb7ae2ded3a69161324ccf1602
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-10-26 12:11:07 -0700

    after lecture, oct 26th

--- lab-elizabeth/app/view/home/home-controller.js
+++ lab-elizabeth/app/view/home/home-controller.js
@@ -2,9 +2,23 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', HomeController ];
+module.exports = ['$log', '$rootscope', 'galleryService', HomeController ];
 
-function HomeController($log){
+function HomeController($log, $rootscope, galleryService){
   $log.debug('init homeCtrl');
-  this.oneAtATime = true;
+  this.galleries = [];
+
+  this.fetchGalleries = function(){
+    galleryService.fetchGalleries()
+    .then(galleries => {
+      this.galleries = galleries;
+    });
+  };
+
+  this.fetchGalleries();
+
+  $rootscope.$on('locationChangeSuccess', () => {
+    this.fetchGalleries();
+  });
+
 }
