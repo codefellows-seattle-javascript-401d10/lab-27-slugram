244af42db5a64a5e3d656ff4a2eb545e9704712d
meeksfred <maxfred0025@gmail.com>
2016-10-25 17:36:01 -0700

    toggle for signup/login working

--- lab-max/app/view/landing/landing-controller.js
+++ lab-max/app/view/landing/landing-controller.js
@@ -6,4 +6,12 @@ module.exports = ['$log', '$location', 'authService', LandingController];
 
 function LandingController($log, $location, authService){
   $log.debug('init LandingController');
+  this.showLogin = false;
+
+  this.loginToggle = function(){
+    if(this.showLogin === false)
+      return this.showLogin = true;
+
+    this.showLogin = false;
+  };
 }
