cb059ff4f6e83c2f4e91234087c6392789e641a1
kaylynyuh <yuhkay4@gmail.com>
2016-10-27 16:22:42 -0700

    adding navbar feature from class

---
+++ lab-kaylyn/app/component/navbar.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  template: require('./navbar.html'),
+  controller: ['$log', '$location', '$rootScope', 'authService', NavController],
+  controllerAs: 'navCtrl',
+  bindings: {
+    appTitle: '@',
+  },
+};
+
+function NavController($log, $location, $rootScope, authService){
+  $log.debug('init navCtrl');
+
+  this.checkPath = function(){
+    let path = $location.path();
+    if (path === '/join'){
+      this.hideButtons = true;
+      authService.getToken()
+      .then(() => {
+        $location.url('/home');
+      });
+    }
+
+    if(path !== '/join'){
+      this.hideButtons = false;
+      authService.getToken()
+      .catch( () => {
+        $location.url('/join#login');
+      });
+    }
+  };
+
+  this.checkPath();
+
+  $rootScope.$on('$locationChangeSuccess', () => {
+    this.checkPath();
+  });
+
+
+  this.logout = function(){
+    $log.log('navbarCtrl.logout()');
+    this.hideButtons = true;
+    authService.logout()
+    .then(() => {
+      $location.url('/');
+    });
+  };
+}
--- lab-kaylyn/app/view/landing/landing-controller.js
+++ lab-kaylyn/app/view/landing/landing-controller.js
@@ -1,20 +1,30 @@
+// 'use strict';
+//
+// require('./_landing.scss');
+//
+// module.exports = ['$log', '$location', 'authService', LandingController];
+//
+// function LandingController($log, $location, authService){
+//   $log.debug('authService.LandingController()');
+//
+//   this.showLogin = false;
+//   this.showSignup = false;
+//
+//   this.login = function(){
+//     this.showLogin ? this.showLogin = false : this.showLogin = true;
+//   };
+//
+//   this.signup = function(){
+//     this.showSignup ? this.showSignup = false : this.showSignup = true;
+//   };
+// }
 'use strict';
 
 require('./_landing.scss');
 
-module.exports = ['$log', '$location', 'authService', LandingController];
+module.exports = ['$log', '$location', '$rootScope', 'authService', LandingController];
 
-function LandingController($log, $location, authService){
-  $log.debug('authService.LandingController()');
-
-  this.showLogin = false;
-  this.showSignup = false;
-
-  this.login = function(){
-    this.showLogin ? this.showLogin = false : this.showLogin = true;
-  };
-
-  this.signup = function(){
-    this.showSignup ? this.showSignup = false : this.showSignup = true;
-  };
+function LandingController($log, $location, authService ){
+  let url = $location.url();
+  this.showSignup = url === '/join#signup' || url === '/join';
 }
