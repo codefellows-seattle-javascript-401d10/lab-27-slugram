21fa463e6b983697f3174aff72abab626568173e
kaylynyuh <yuhkay4@gmail.com>
2016-11-01 15:51:20 -0700

    login test for authService passing

--- lab-kaylyn/test/auth-service-test.js
+++ lab-kaylyn/test/auth-service-test.js
@@ -4,8 +4,9 @@ describe('testing authService', function(){
 
   beforeEach(() => {
     angular.mock.module('demoApp');
-    angular.mock.inject((authService, $httpBackend) => {
+    angular.mock.inject((authService, $window, $httpBackend) => {
       this.authService = authService;
+      this.$window = $window;
       authService.setToken('1234');
 
       this.authService = authService;
@@ -23,13 +24,37 @@ describe('testing authService', function(){
 
       let headers = {
         Accept: 'application/json',
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
       };
 
       this.$httpBackend.expectPOST('http://localhost:3000/api/signup', user, headers)
       .respond(200, {token: '666'});
 
       this.authService.signup(user);
+      this.$httpBackend.flush();
+    });
+  });
+
+  describe('testing authService service.login', () => {
+    it('should authorize a returning user', () => {
+      let user = {
+        name: 'userName',
+        password: 'userPassword',
+      };
+      let base64 = this.$window.btoa(`${user.username}:${user.password}`);
+      let headers = {
+        Accept: 'application/json',
+        Authorization: `Basic ${base64}`,
+      };
+
+      this.$httpBackend.expectGET('http://localhost:3000/api/login', headers)
+      .respond(200, '666');
+
+      this.authService.login(user)
+      .then(token => {
+        expect(token).toBe('666');
+      });
+
       this.$httpBackend.flush();
     });
   });
