c86da77eb3ccee77ccf2ff76b7336994650ef501
JudyVue <judyvue87@gmail.com>
2016-11-07 17:41:45 -0800

    playing with CSS and built in Angular directives and failing

--- lab-judy/app/component/gallery/thumbnail-container/thumbnail-container.js
+++ lab-judy/app/component/gallery/thumbnail-container/thumbnail-container.js
@@ -1,9 +1,17 @@
 'use strict';
 
+require('./_thumbnail-container.scss');
+
 module.exports = {
   template: require('./thumbnail-container.html'),
+  controller: ['$log', 'picService', ThumbnailContainerController],
   controllerAs: 'thumbnailContainerCtrl',
   bindings: {
     gallery: '<',
   },
 };
+
+function ThumbnailContainerController(){
+  //trying to figure out how to get this to hide and show according to the clicking of pic glyphicon
+  this.showThumbnailContainer = false;
+}
--- lab-judy/app/filter/item-search.js
+++ lab-judy/app/filter/item-search.js
@@ -1,34 +1,34 @@
 'use strict';
 
-module.exports = function(){
-  return function(items, searchTerm){
-    let fuzzyRegex = generateFuzzyRegex(searchTerm);
-
-    let filteredByName = items.filter(item => {
-      //does the generateFuzzyRegex happen here or next block?
-      let fuzzyName = fuzzyRegex.test(item.name.toUpperCase());
-      let fuzzyDesc = fuzzyRegex.test(item.desc.toUpperCase());
-      return fuzzyName;
-    });
-    filtered.filter(item => {
-      //any chaining of methods?
-      return item;
-    });
-  };
-};
-
 // module.exports = function(){
 //   return function(items, searchTerm){
 //     let fuzzyRegex = generateFuzzyRegex(searchTerm);
 //
-//     return items.filter(item => {
+//     let filteredByName = items.filter(item => {
+//       //does the generateFuzzyRegex happen here or next block?
 //       let fuzzyName = fuzzyRegex.test(item.name.toUpperCase());
 //       let fuzzyDesc = fuzzyRegex.test(item.desc.toUpperCase());
-//       //run filter on the name, return that, then f/u with filter by desc and return that
-//       return fuzzyName || fuzzyDesc;
+//       return fuzzyName;
+//     });
+//     filtered.filter(item => {
+//       //any chaining of methods?
+//       return item;
 //     });
 //   };
 // };
+
+module.exports = function(){
+  return function(items, searchTerm){
+    let fuzzyRegex = generateFuzzyRegex(searchTerm);
+
+    return items.filter(item => {
+      let fuzzyName = fuzzyRegex.test(item.name.toUpperCase());
+      let fuzzyDesc = fuzzyRegex.test(item.desc.toUpperCase());
+      //run filter on the name, return that, then f/u with filter by desc and return that
+      return fuzzyName || fuzzyDesc;
+    });
+  };
+};
 
 function generateFuzzyRegex(input){
   if (!input) return /.*/;
