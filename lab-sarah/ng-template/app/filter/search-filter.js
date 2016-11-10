9556a0a14ef2e02baa8e0da0c148ae8aaace7238
Sarah DeBey <sarah.debey@gmail.com>
2016-11-09 16:23:56 -0800

    refactor search-filter

--- lab-sarah/ng-template/app/filter/search-filter.js
+++ lab-sarah/ng-template/app/filter/search-filter.js
@@ -1,34 +1,11 @@
 'use strict';
 
-module.exports = function(){
+module.exports = function() {
   return function(assets, nameSearchTerm, descSearchTerm){
-    let fuzzyRegex;
-
-    if (!nameSearchTerm && !descSearchTerm) {
-      fuzzyRegex = generateFuzzyRegex();
-      return assets.filter(asset => {
-        return fuzzyRegex.test(asset.name.toUpperCase());
-      });
-    }
-
-    if (nameSearchTerm && descSearchTerm){
-      return assets.filter(function(asset) {
-        return fuzzyRegex.test(asset.name.toUpperCase());
-      }).filter(function(asset){
-        return fuzzyRegex.test(asset.desc.toUpperCase());
-      });
-    }
-
-    if (!nameSearchTerm) {
-      fuzzyRegex = generateFuzzyRegex(descSearchTerm);
-      return assets.filter(asset => {
-        return fuzzyRegex.test(asset.desc.toUpperCase());
-      });
-    }
-
-    fuzzyRegex = generateFuzzyRegex(nameSearchTerm);
-    return assets.filter(asset => {
-      return fuzzyRegex.test(asset.name.toUpperCase());
+    return assets.filter(function(asset){
+      return generateFuzzyRegex(nameSearchTerm).test(asset.name.toUpperCase());
+    }).filter(function(asset){
+      return generateFuzzyRegex(descSearchTerm).test(asset.desc.toUpperCase());
     });
   };
 };
